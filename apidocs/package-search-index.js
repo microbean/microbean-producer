2e230308396c23287b7fee2b7b21b4f0ae39a5a2
microbean <ci@microbean.org>
2025-12-20 06:24:43 +0000

    Site checkin for project microBean™ Producer

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.microbean.producer","l":"org.microbean.producer"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"org.microbean.producer","l":"org.microbean.producer"}];updateSearchResults();
